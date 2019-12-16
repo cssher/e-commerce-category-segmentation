a05482486fd149d5b80e086085e0f61b91fcc5be
Sher Ali <sherali2910.sa@gmail.com>
2019-12-17 00:11:17 +0530

    Updates

---
+++ precache-manifest.f21aff7b90e98359267d61cbcff93a29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c5a13276ce3b45ea333cb79e967048",
+    "url": "/e-commerce-category-segmentation/index.html"
+  },
+  {
+    "revision": "6fe26da71e91471e1f2b",
+    "url": "/e-commerce-category-segmentation/static/css/2.db46f67d.chunk.css"
+  },
+  {
+    "revision": "8992851ebe6c3742227c",
+    "url": "/e-commerce-category-segmentation/static/css/main.bcd1cba6.chunk.css"
+  },
+  {
+    "revision": "6fe26da71e91471e1f2b",
+    "url": "/e-commerce-category-segmentation/static/js/2.d6cecb7f.chunk.js"
+  },
+  {
+    "revision": "6650868d695d4b8de1caf3543757beb4",
+    "url": "/e-commerce-category-segmentation/static/js/2.d6cecb7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8992851ebe6c3742227c",
+    "url": "/e-commerce-category-segmentation/static/js/main.c24ca065.chunk.js"
+  },
+  {
+    "revision": "475f125798896e27c602",
+    "url": "/e-commerce-category-segmentation/static/js/runtime-main.f3817855.js"
+  }
+]);
